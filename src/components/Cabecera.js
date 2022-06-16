378cff7c1b43844e06ff132a372fc3bb5cfe7616
Beatu0429 <nievesb0429@gmail.com>
2022-06-15 21:02:11 -0500

    Agregando colores

--- src/components/Cabecera.js
+++ src/components/Cabecera.js
@@ -1,13 +1,14 @@
-import React from 'react'
-import estilos from "./Cabecera.module.css"
+import React from "react";
+import estilos from "./Cabecera.module.css";
 
-
-export default function Cabecera({cantidad}) {
-  
+export default function Cabecera({ cantidad }) {
   return (
-    <header  className={estilos.encabezado}>
-        <h1 className={estilos.titulo}>Carrito de compras</h1>
-        <p >cantidad de productos <span className={estilos.descripcion}>{cantidad}</span></p>
+    <header className={estilos.encabezado}>
+      <h1 className={estilos.titulo}>Carrito de compras</h1>
+      <p>
+        cantidad de productos{" "}
+        <span className={estilos.descripcion}>{cantidad}</span>
+      </p>
     </header>
-  )
+  );
 }
